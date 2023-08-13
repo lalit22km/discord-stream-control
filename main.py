4508214ffc0bc2458da411538419b2efb77730bf
Lalit Kumar <61497570+lalit22km@users.noreply.github.com>
2023-08-13 22:38:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,9 +11,13 @@ token = os.getenv('DISCORD_TOKEN')
 driver = webdriver.Firefox()
 
 def youtube(query):
-    search=query
     response = requests.get('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=% s&type=video&key=% s' % (query, ytkey))
-    return
+    response_json = json.loads(response.text)
+    videoid = response_json['items'][0]['id']['videoId']
+    title = response_json['items'][0]['snippet']['title']
+    thumb = response_json['items'][0]['snippet']['thumbnails']['default']['url']
+    url = "https://www.youtube.com/watch?v=" + videoid
+    return title, thumb, url
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -24,15 +28,20 @@ class MyClient(discord.Client):
         if message.author == self.user:
             return
 
-        if message.content.startswith('!goto'):
+        if message.content.startswith('!g'):
             await message.reply('Hello!', mention_author=True)
-            link = message.content.replace('!goto ', '')
+            link = message.content.replace('!g ', '')
             driver.get(link)
-        if message.content.startswith('!play'):
+        if message.content.startswith('!p'):
             await message.reply('Playing..', mention_author=True)
             actions = ActionChains(driver)
             actions.send_keys('k')
             actions.perform()
+        if message.content.startswith('!s'):
+            query = message.content.replace('!s ','')
+            title, thumb, url = youtube(query)
+            await message.reply('https://gasaisb.net/embed.php?title=% s&image=% s' % (title.replace(' ','%20'), thumb))
+            driver.get(url)
 
 client = MyClient()
 client.run(token)
